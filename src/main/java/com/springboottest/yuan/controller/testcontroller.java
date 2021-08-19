a8eeaa609f222e4e7de127946bc64b2295e04d62
LinZY1004 <57659597+LinZY1004@users.noreply.github.com>
2021-08-19 10:48:09 +0800

    删除

--- src/main/java/com/springboottest/yuan/controller/testcontroller.java
+++ src/main/java/com/springboottest/yuan/controller/testcontroller.java
@@ -7,12 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class testcontroller {
     @GetMapping("/test")
     public String test(){
-        System.out.println("hhhh");
-        System.out.println("h22hhh66");
-        System.out.println("hhh3333333333333h66");
-        System.out.println("hhhh66");
-        System.out.println("hhhh66");
-        System.out.println("hhh2343h66");
         return "index.html";
 
     }
