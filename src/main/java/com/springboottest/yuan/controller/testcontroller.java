53cbe28e5612d99141ab253ac9a39916f8fb7559
LinZY1004 <57659597+LinZY1004@users.noreply.github.com>
2021-08-19 10:36:26 +0800

    Update testcontroller.java
    第二次修改

--- src/main/java/com/springboottest/yuan/controller/testcontroller.java
+++ src/main/java/com/springboottest/yuan/controller/testcontroller.java
@@ -8,7 +8,7 @@ public class testcontroller {
     @GetMapping("/test")
     public String test(){
         System.out.println("hhhh");
-        System.out.println("hhhh66");
+        System.out.println("h22hhh66");
         System.out.println("hhhh66");
         System.out.println("hhhh66");
         System.out.println("hhhh66");
