73a03337488aa75ebc2c48a7b054559c0d593284
LinZY1004 <57659597+LinZY1004@users.noreply.github.com>
2021-08-19 10:52:51 +0800

    远程修改

--- src/main/java/com/springboottest/yuan/controller/testcontroller.java
+++ src/main/java/com/springboottest/yuan/controller/testcontroller.java
@@ -10,9 +10,6 @@ public class testcontroller {
         System.out.println("hhhh");
         System.out.println("h22hhh66");
         System.out.println("hhh333333333333h66");
-        System.out.println("hhhh66");
-        System.out.println("hhhh66");
-        System.out.println("hhh43h6226");
         return "index.html";
 
     }
