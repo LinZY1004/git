d777a8c46027f949456e4dc7d8d21c3def709182
XiaoYuan00 <1353967196@qq.com>
2021-08-19 09:43:39 +0800

    test

---
+++ src/main/java/com/springboottest/yuan/YuanApplication.java
@@ -0,0 +1,13 @@
+package com.springboottest.yuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboottest/yuan/YuanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboottest.yuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
